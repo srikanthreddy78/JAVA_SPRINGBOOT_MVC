be04cb650e5e2fa4fd8414ec6eb717e53d51065e
srikanth <gsrikanthreddy78@gmail.com>
2023-02-21 18:50:09 +0530

    initial commit

---
+++ src/com/hello/TopicService.java
@@ -0,0 +1,36 @@
+package com.hello;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class TopicService {
+
+    @Autowired
+    private TopicRepository topicRepository;
+    public List<Topics> getAllTopics(){
+        List<Topics> topics1  = new ArrayList<>();
+        topicRepository.findAll().forEach(topics1::add);
+        return topics1;
+    }
+
+    public Topics getTopic(String id){
+        return topicRepository.findOne(id);
+    }
+
+    public void addTopic(Topics topic){
+        topicRepository.save(topic);
+    }
+
+    public void updateTopic(Topics topic, String id) {
+        topicRepository.save(topic);
+    }
+
+    public void deleteTopic(String id) {
+        topicRepository.delete(id);
+    }
+}
---
+++ src/com/hello/Topics.java
@@ -0,0 +1,42 @@
+package com.hello;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Topics {
+    @Id
+    private String id;
+
+    private String name;
+
+    private String desc;
+
+    public Topics() {
+    }
+
+    public Topics(String id, String name, String desc) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.desc = desc;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
