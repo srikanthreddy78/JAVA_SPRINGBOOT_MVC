e1eb2cb8613442b3078c8adccb1bc6dec827e6a9
srikanthreddy78 <41779775+srikanthreddy78@users.noreply.github.com>
2023-03-18 16:57:13 +0530

    Add files via upload

---
+++ hellocontroller.java
@@ -0,0 +1,39 @@
+package com.mvc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class HelloController {
+
+    @Autowired
+    private TopicService topicService;
+    @RequestMapping("/topics")
+    public List getAllTopics(){
+        return topicService.getAllTopics();
+    }
+
+    @RequestMapping("/topics/{id}")
+    public Topics getTopic(@PathVariable String id){
+        return topicService.getTopic(id);
+    }
+
+    @RequestMapping(method=RequestMethod.POST,value = "/topics")
+    public void addTopic(@RequestBody Topics topic){
+        topicService.addTopic(topic);
+    }
+
+    @RequestMapping(method=RequestMethod.PUT,value = "/topics/{id}")
+    public void updateTopic(@RequestBody Topics topic,@PathVariable String id){
+        topicService.updateTopic(topic,id);
+    }
+
+    @RequestMapping(method=RequestMethod.DELETE,value = "/topics/{id}")
+    public void deleteTopic(@PathVariable String id){
+        topicService.deleteTopic(id);
+    }
+
+
+}
---
+++ topicrepo.java
@@ -0,0 +1,7 @@
+package com.mvc;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TopicRepository extends CrudRepository<Topics,String> {
+
+}
